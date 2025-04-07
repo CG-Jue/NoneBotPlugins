3306b5f83dd8e8225413cf978995a41c66d666e7
CGJUE <131164348+CG-Jue@users.noreply.github.com>
2025-04-07 16:05:52 +0800

    =-=

--- CTF/config.py
+++ CTF/config.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
+import nonebot
 
+'''
+考虑到开源，可能有信息泄漏（虽然没啥用），所以不直接写在代码里
+'''
 
 class Config(BaseModel):
     CONFIG: dict = {
-        # "SEND_LIST": [629590326,445629724], # 推送的群号列表 测试
-        "SEND_LIST": [936493920,391680981], # 推送的群号列表
-        "SEND_TIME": 1,                     # 推送时间差值，即距离报名开始多少天推送
+
+        "SEND_LIST": [], # 推送的群号列表
+
+        "SEND_TIME": 1,      # 推送时间差值，即距离报名开始多少天推送
         }
